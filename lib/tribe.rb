47497a85e823fb2d8e239152175bfbf2caa92719
KLSimmons <klsimmons88@gmail.com>
2016-06-11 12:11:29 -0400

    Create Tribe class and pass tests in tribe_test.rb

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,9 +1,22 @@
 class Tribe
   attr_reader :name, :members
 
-  def initialize(name)
-    @name = name
-    @members = members
+  def initialize(options={})
+    @name = options[:name]
+    @members = options[:members]
+    @immune_array = @members
     puts "#{@name}"
+  end
+
+  def to_s
+    @name
+  end
+
+  def tribal_council(immune)
+    @immune = immune[:immune]
+    @immune_array = [@members, @immune]
+    if @immune_array[0].include?(@immune)
+      @immune_array[0].pop
+    end
   end
 end
